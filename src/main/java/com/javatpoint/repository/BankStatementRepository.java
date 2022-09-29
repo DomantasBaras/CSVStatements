25dc865ae908ab8aecc97dbfccdcfaf91e8762b3
DomantasBaras <Domantas11@yahoo.com>
2022-09-30 00:26:52 +0300

    First Commit

--- src/main/java/com/javatpoint/repository/BankStatementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javatpoint.repository;
-
-import com.javatpoint.model.BankStatement;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BankStatementRepository extends CrudRepository<BankStatement, Integer>
-{
-}
